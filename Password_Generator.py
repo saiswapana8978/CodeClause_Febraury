2769ce2a22885175d38b19858f2d8562060d9f57
saiswapana8978 <133654843+saiswapana8978@users.noreply.github.com>
2024-02-25 15:43:45 +0530

    Update Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,30 +1,59 @@
+from tkinter import *
+import string
 import random
-letters=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u',
-         'v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P',
-         'Q','R','S','T','U','V','W','X','Y','Z']
-numbers=['1','2','3','4','5','6','7','8','9']
-symbols=['!','@','#','$','%','^','&','*']
-print("Hello, Welcome to the Password Generator!!")
-length=int(input("Enter the length of the password you want: \t"))
-n_letters=int(input("Enter how many letters you want: \t"))
-n_numbers=int(input("Enter how many numbers you want: \t"))
-n_symbols=int(input("Enter how many symbols you want: \t"))
-total_length=(n_letters)+(n_numbers)+(n_symbols)
-password_list=[]
-if length==total_length:
-    for i in range(1,n_letters+1):
-        char = random.choice(letters)
-        password_list+=char
-    for i in range(1,n_numbers+1):
-        char=random.choice(numbers)
-        password_list+=char
-    for i in range(1,n_symbols+1):
-        char=random.choice(symbols)
-        password_list+=char
-    random.shuffle(password_list)
-    print("".join(password_list))
-else:
-    print("The Total Characters length should have to match with your Password length")
+import pyperclip
 
+def generator():
+    small_alphabets = string.ascii_lowercase
+    capital_alphabets = string.ascii_uppercase
+    numbers = string.digits
+    special_characters = string.punctuation
+
+    all = small_alphabets + capital_alphabets + numbers + special_characters
+    password_length = int(length_Box.get())
+
+    if choice.get() == 1:
+        passwordField.insert(0,random.sample(small_alphabets, password_length))
+    if choice.get() == 2:
+        passwordField.insert(0, random.sample(small_alphabets+capital_alphabets, password_length))
+    if choice.get() == 3:
+        passwordField.insert(0,random.sample(all, password_length))
+
+def copy():
+    random_password=passwordField.get()
+    pyperclip.copy(random_password)
+
+root = Tk()
+root.config(bg='gray20')
+
+choice = IntVar()
+Font = ('arial',13,'bold')
+
+passwordLabel = Label(root, text='Password Generator', font=('times new roman',20,'bold'), bg='gray20', fg='white')
+passwordLabel.grid(pady=10)
+
+weakRadioButton = Radiobutton(root, text='Weak', value=1, variable=choice, font=Font)
+weakradioButton.grid(pady=5)
+mediumRadioButton = Radiobutton(root, text='Medium', value=2, variable=choice, font=Font)
+mediumradioButton.grid(pady=5)
+strongRadioButton = Radiobutton(root, text='Strong', value=3, variable=choice, font=Font)
+strongradioButton.grid(pady=5)
+
+lengthLabel = Label(root, text='Password Length', font=('times new roman',20,'bold'), bg='gray20', fg='white')
+lengthLabel.grid(pady=5)
+
+length_Box = Spinbox(root, from_=5, to=18, width=5, font=Font)
+length_Box.grid(pady=5)
+
+generateButton = Button(root, text='Generate', font=Font, command=generator)
+generateButton.grid(pady=5)
+
+passwordField = Entry(root, width=25, bd=2, font=Font)
+passwordField.grid()
+
+copyButton = Button(root, text='copy', font=Font, command=copy)
+copyButton.grid(pady=5)
+
+root.mainloop()
 
 
